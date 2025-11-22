0c7d6a8f67152db16c1d41abb697dea0fdbbdd1f
Wise Munene Mutuku <wisemunene99@gmail.com>
2025-11-22 12:24:22 +0300

    done

---
+++ lib/insert.sql
@@ -0,0 +1,10 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES
+('Mr. Chocolate', 12, 'M', 'Brown', 'Calm', 1),
+('Rowdy', 8, 'M', 'Black', 'Aggressive', 1),
+('Tabitha', 5, 'F', 'Black', 'Calm', 1),
+('Sergeant Brown', 17, 'M', 'Brown', 'Strict', 1),
+('Melissa', 6, 'F', 'White', 'Happy', 1),
+('Grinch', 3, 'M', 'Green', 'Grumpy', 1),
+('Wendy', 4, 'F', 'Brown', 'Playful', 1),
+(NULL, 2, 'M', 'Grey', 'Unknown', 1);
